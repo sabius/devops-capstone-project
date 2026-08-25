b1db5926489ad989725c4b834bc91c0ef7732c22
Gonzalo Daniel <sabius@gmail.com>
2026-08-24 20:41:11 -0400

    fixing typo

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -67,7 +67,7 @@ class TestAccountService(TestCase):
             "Referrer-Policy": "strict-origin-when-cross-origin",
         }
         for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)sssssssssssssssssssssssssssss
+            self.assertEqual(response.headers.get(key), value)
 
 
     ######################################################################
